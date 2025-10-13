86aee555dcf1fdee5c32802d6e65f8263e72df53
LianisDevs <lianisdevs@gmail.com>
2025-10-14 10:56:26 +1100

    Updated 4-changing back to last version

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-/* 
- *main- Where it all starts
- *Description: This function is the entry point for starting the application. It initialises the program and calls other function within to do specific tasks. 
-*Return: 0 is success
-*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
