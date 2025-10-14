12979b5cd03a030a4ea150754e6977c7a1da9b98
LianisDevs <lianisdevs@gmail.com>
2025-10-14 12:05:25 +1100

    Added comments

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main- entry point
+ *
+ * return- always zero to signify success
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	/**
+	 * return - always zero to show success
+	 */
 	return (0); }
