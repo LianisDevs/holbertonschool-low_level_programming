be445db2feabae66401ba7e4607ddf4eb47906c3
LianisDevs <lianisdevs@gmail.com>
2025-10-14 12:26:45 +1100

    Updated 4- comments should be right for Betty now

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
- * main- entry point
+ * main - entry point
  *
- * return- always zero to signify success
+ * Return: always zero to signify success
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	/**
-	 * return - always zero to show success
-	 */
 	return (0); }
