1b3d0e09a44139a3d5e1c0a1fb0603ffd75978b4
LianisDevs <lianisdevs@gmail.com>
2025-10-19 20:41:26 +1100

    Add 10- Add

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ * @n: The number to check
+ *
+ * Return: nothing
+ */
+
+void jack_bauer(void)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (n * (-1));
+	}
+	else
+	{
+		return (0);
+	}
+}
