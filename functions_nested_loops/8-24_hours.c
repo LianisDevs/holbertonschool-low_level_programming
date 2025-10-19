157bff0733beabbc48a1540a1cbeeb09151b2c0e
LianisDevs <lianisdevs@gmail.com>
2025-10-19 23:41:45 +1100

    Add 8- 24 hours

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,23 +1,36 @@
 #include "main.h"
 /**
  * jack_bauer - prints every minute of the day
- * @n: The number to check
  *
  * Return: nothing
  */
 
 void jack_bauer(void)
 {
-	if (n > 0)
+	int min_r;
+	int min_l;
+	int hour_r;
+	int hour_l;
+
+	for (hour_l = 0; hour_l <= 2; hour_l++)
 	{
-		return (n);
-	}
-	else if (n < 0)
-	{
-		return (n * (-1));
-	}
-	else
-	{
-		return (0);
+		for (hour_r = 0; hour_r <= 9 && hour_l <= 2; hour_r++)
+		{
+			if (hour_l == 2 && hour_r == 4)
+				break;
+
+			for (min_l = 0; min_l <= 5; min_l++)
+			{
+				for (min_r = 0; min_r <= 9; min_r++)
+				{
+					_putchar('0' + hour_l);
+					_putchar('0' + hour_r);
+					_putchar(':');
+					_putchar('0' + min_l);
+					_putchar('0' + min_r);
+					_putchar('\n');
+				}
+			}
+		}
 	}
 }
---
+++ functions_nested_loops/8-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: always zero to show success
+ */
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
