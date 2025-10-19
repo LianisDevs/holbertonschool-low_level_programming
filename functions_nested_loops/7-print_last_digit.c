3d3202c7bc1d3f79521c4bf6d5093f0a1ec0893a
LianisDevs <lianisdevs@gmail.com>
2025-10-19 22:22:27 +1100

    Updated 7- fixed error

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,20 +8,19 @@
 
 int print_last_digit(int n)
 {
-
-	int last_digit = _abs(n % 10);
-
 	if (n > 0)
 	{
+		int last_digit = n % 10;
 
 		_putchar('0' + last_digit);
 		return (last_digit);
 	}
 	else if (n < 0)
 	{
+		int last_digit = n % 10;
 
-		_putchar('0' + last_digit);
-		return (last_digit);
+		_putchar('0' + last_digit * -1);
+		return (last_digit * -1);
 	}
 	else
 	{
