db855233e618c05413c8d0087d865682dea76aeb
LianisDevs <lianisdevs@gmail.com>
2025-10-19 17:35:44 +1100

    Updated 1- fixed for betty

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * main - entry point
+ * print_alphabet - prints the alphabet from a to z using _putchar
  *
- * Return: always zero to show success
+ * Return: nothing
  */
 
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
 	}
 	_putchar('\n');
-	return;
 }
