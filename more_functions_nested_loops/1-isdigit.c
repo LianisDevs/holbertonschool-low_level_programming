913499b15b064df5143bce3ba9859a1b1145e1fc
LianisDevs <lianisdevs@gmail.com>
2025-10-20 11:32:02 +1100

    Add 1- is digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - checks for digit 0-9
+ * @c: Variable to check
+ *
+ * Return: 1 if c is digit, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if ((c >= 48) && (c <= 57))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
