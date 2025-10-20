f7a4fb42dee16c9cb127b483c2166718bc0c2675
LianisDevs <lianisdevs@gmail.com>
2025-10-20 21:46:28 +1100

    Add- 11- print to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints all nat numbers from n to 98
+ * @n: number passed to function
+ *
+ * Return: nothing
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		int length = ((98 - n) + 1);
+
+		for (int i = 0; i < length; i++)
+		{
+			printf("%d", n);
+			if (n == 98)
+			{
+				break;
+			}
+			printf(", ");
+			n++;
+		}
+	printf("\n");
+	}
+	else
+	{
+		int length = ((n - 98) + 1);
+
+		for (int i = 0; i < length; i++)
+		{
+			printf("%d", n);
+			if (n == 98)
+			{
+				break;
+			}
+			printf(", ");
+			n--;
+		}
+	printf("\n");
+	}
+}
