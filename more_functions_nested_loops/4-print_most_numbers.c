5a4f00fddfb62541a0a0f00350e6595a6b8ce4b8
LianisDevs <lianisdevs@gmail.com>
2025-10-20 12:14:36 +1100

    Add 4- print most numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers 0-9 with newline except 2 & 4
+ *
+ * Return: nothing void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if ((i == 2) || (i == 4))
+		{
+			continue;
+		}
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
