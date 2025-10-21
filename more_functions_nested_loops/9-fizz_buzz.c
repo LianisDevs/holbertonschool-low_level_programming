9b8044a6a11be8de273b202779810e4651cc622f
LianisDevs <lianisdevs@gmail.com>
2025-10-21 11:36:04 +1100

    Add - 9 Fizz Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int number;
+	int total = 100;
+
+	for (number = 1; number <= total; number++)
+	{
+		if ((number % 3 != 0) && (number % 5 != 0))
+		{
+			printf("%d", number);
+		}
+		else if ((number % 3 == 0) && (number % 5 != 0))
+		{
+			printf("Fizz");
+
+		}
+		else if ((number % 5 == 0) && (number % 3 != 0))
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("FizzBuzz");
+		}
+
+		if (number < total)
+		{
+			printf(" ");
+		}
+		else
+		{
+			printf("\n");
+		}
+	}
+}
