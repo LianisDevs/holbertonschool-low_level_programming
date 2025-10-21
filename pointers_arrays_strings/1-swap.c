c88a5ca5377dc89dad3d23428c2383a64c15e8cc
LianisDevs <lianisdevs@gmail.com>
2025-10-21 21:12:40 +1100

    Add 1- swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swap values of two ints
+ * @a: first value
+ * @b: second value
+ *
+ * Return: nothing void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
