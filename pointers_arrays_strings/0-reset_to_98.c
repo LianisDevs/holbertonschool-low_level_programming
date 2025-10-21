083f661ace16ddd63c6742d4008960f653c64188
LianisDevs <lianisdevs@gmail.com>
2025-10-21 21:02:13 +1100

    Updated 0- comment for betty

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 - updates value it points to
- * @*n: pointer to value of variable n
+ * @n: variable
  *
  * Description: Take a pointer to an int and updates the value
  * it point to, to 98.
