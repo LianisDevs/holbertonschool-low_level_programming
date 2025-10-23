a15e9beb34c293832e6dc2d951f4ea59ef8812f2
LianisDevs <lianisdevs@gmail.com>
2025-10-23 15:30:41 +1100

    Updated 6-puts2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * puts2 - prints every other character
- *
+ * @str: what we're checking
  * Return: nothing
  */
 
@@ -9,7 +9,7 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while ((i == 0) || (str[i - 1] != '\0' && str[i] != '\0'))
 	{
 		_putchar(str[i]);
 		i += 2;
