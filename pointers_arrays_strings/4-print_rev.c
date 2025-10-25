ea5a0f7a1e8ffd99993f1b8ba0126de8f71d35da
LianisDevs <lianisdevs@gmail.com>
2025-10-25 14:00:51 +1100

    Add 4-print rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: pointer to string
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int counter = 0;
+	int i;
+
+	while (s[counter] != '\0')
+	{
+		counter++;
+	}
+
+	while (counter >= 0)
+	{
+		_putchar(s[counter]);
+		counter--;
+	}
+	_putchar('\n');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -13,4 +13,6 @@ void puts2(char *str);
 
 void _puts(char *str);
 
+void print_rev(char *s);
+
 #endif
