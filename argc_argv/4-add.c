c626ab1c234622a07e4fba7dff4bdfa23905f839
LianisDevs <lianisdevs@gmail.com>
2025-11-02 15:54:53 +1100

    Add 4-add

---
+++ argc_argv/4-add.c
@@ -0,0 +1,62 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int validate_input(int argc, char *argv[]);
+
+/**
+ * main - multiplies 2 numbers
+ * @argc: arg count
+ * @argv: arg value
+ * Return: 0 to show success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (1);
+	}
+
+	if (validate_input(argc, argv) == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+		sum += atoi(argv[i]);
+
+	printf("%d\n", sum);
+
+	return (0);
+}
+
+/**
+ * validate_input - checks input is all numbers
+ * @argc: arg count
+ * @argv: arg value
+ * Return: 0 if only numbers, 1 if contains not numbers
+ */
+
+int validate_input(int argc, char *argv[])
+{
+	int i, j;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] >= '0' && argv[i][j] <= '9')
+				continue;
+			else
+				return (1);
+		}
+	}
+	return (0);
+}
