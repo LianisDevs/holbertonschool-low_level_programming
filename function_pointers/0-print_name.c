10b11599ffbb19c4e22fcb535d77d83cae9b15a0
LianisDevs <lianisdevs@gmail.com>
2025-11-10 17:52:33 +1100

    Add 0-print name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - function that prints name
+ * @name: pointer to string called name
+ * @f: pointer to function
+ * Return: nothing/ void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+		return;
+	f(name);
+}
