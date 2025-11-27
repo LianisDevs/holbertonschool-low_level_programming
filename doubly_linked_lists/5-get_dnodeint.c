531bba575faed8e71962881788ee306538bfe644
LianisDevs <lianisdevs@gmail.com>
2025-11-27 22:25:22 +1100

    Udpated 5- betty comment error

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	/*
 	 * checking while num_elements < index it'll either match
 	 * or increment through to last node therefore == NULL
-	 * by doing it this way when we rteturn head it will 
+	 * by doing it this way when we return head it will
 	 * either be the index or it will be NULL
 	 */
 	while (num_elements < index && head != NULL)
