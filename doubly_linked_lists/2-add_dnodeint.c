6f2cee2d6b54476ae6dbffef9c8b0ec484bbae56
LianisDevs <lianisdevs@gmail.com>
2025-11-27 19:03:42 +1100

    Updated 2- set new_node->next->prev to be new_node

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -40,7 +40,12 @@ dlistint_t *node_setup(dlistint_t **head, const int n, dlistint_t *new_node)
 	new_node->n = n;
 	new_node->prev = NULL;
 
-	new_node->next = *head;
-
+	if (*head == NULL)
+		new_node->next = NULL;
+	else
+	{
+		new_node->next = *head;
+		new_node->next->prev = new_node;
+	}
 	return (new_node);
 }
