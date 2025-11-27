f2c9a07a7acb854c81e90314e9e3ee45226acaf0
LianisDevs <lianisdevs@gmail.com>
2025-11-27 19:13:53 +1100

    Added: 3-add_dnodeint_end.c Updated: readme and lists.h to include new function

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+dlistint_t *node_setup(const int n, dlistint_t *new_node, dlistint_t *temp);
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	
+	dlistint_t *temp = *head;
+	
+	if (new_node == NULL)
+		return (NULL);
+	
+	while (temp->next != NULL)
+		temp = temp->next;
+	/*
+	 * update head to be new node
+	 * node_setup returns the new node
+	 */
+	*head = node_setup(n, new_node, temp);
+
+	return (new_node);
+}
+
+/**
+ * node_setup - sets up the elements of the new node
+ * @temp: copy of head which is a pointer to start of list
+ * @n: int value to add to new node
+ * @new_node: pointer to new node
+ * Return: pointer to new node
+ */
+
+dlistint_t *node_setup(const int n, dlistint_t *new_node, dlistint_t *temp)
+{
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*temp == NULL)
+		new_node->prev = NULL;
+	else
+	{
+		new_node->prev = temp;
+		new_node->prev->next = new_node;
+	}
+	
+	return (new_node);
+}
--- doubly_linked_lists/README.md
+++ doubly_linked_lists/README.md
@@ -5,3 +5,5 @@
 * 1- returns the number of elements in a doubly linked list
 
 * 2- adds a new node to the beginning of the doubly linked list
+
+* 3- adds a new node to the end of the doubly linked list
