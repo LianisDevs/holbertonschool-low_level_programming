2d28102602b432274e48309b928dd42ec81462cb
LianisDevs <lianisdevs@gmail.com>
2025-11-27 12:26:54 +1100

    Added: 2-add_dnodeint.c and Updated: readme.md and lists.h to include enw function

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+dlistint_t *node_setup(dlistint_t **head, const int n, dlistint_t *new_node);
+
+/**
+ * add_dnodeint - add a new node at the beginning of the list
+ * @head: pointer to the first node in the list
+ * @n: value to add within new node
+ * Return: pointer to head
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	/*
+	 * update head to be the new node so it's first in the list
+	 * node_setup returns the new node
+	 */
+	*head = node_setup(head, n, new_node);
+
+	return (new_node);
+}
+
+/**
+ * node_setup - sets up the elements of the new node
+ * @head: pointer to head of list
+ * @n: int value to add to new node
+ * @new_node: pointer to new node
+ * Return: pointer to new node
+ */
+
+dlistint_t *node_setup(dlistint_t **head, const int n, dlistint_t *new_node)
+{
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+
+	return (new_node);
+}
--- doubly_linked_lists/README.md
+++ doubly_linked_lists/README.md
@@ -2,4 +2,6 @@
 
 * 0- prints all elements in a doubly linked list
 
-* 1- returns the number of elements ina doubly linked list
+* 1- returns the number of elements in a doubly linked list
+
+* 2- adds a new node to the beginning of the doubly linked list
