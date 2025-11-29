6120b754aa779ac20975685f9d2ac129646ed359
LianisDevs <lianisdevs@gmail.com>
2025-11-29 22:46:19 +1100

    Updated to free temp in each function that gets to that stage

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -35,7 +35,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		temp = temp->next;
 		num_nodes++;
 	}
-	
+
 	/*check to see if index wasn't found and return to reduce wasted time*/
 	if (num_nodes != index)
 		return (-1);
@@ -43,7 +43,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	/*check if deleting at end*/
 	if (num_nodes == index && temp == NULL)
 		return (delete_at_end(temp));
-	
+
 	/*check if deleting in the somewhere in middle zone*/
 	if (num_nodes == index)
 		return (delete_at_index(temp));
@@ -51,13 +51,26 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	return (-1);
 }
 
+/**
+ * delete_at_start_list - deletes node at start
+ * @temp: at indexed position for node to delete
+ * @head: pointer to head
+ * Return: 1 for success
+ */
 int delete_at_start_list(dlistint_t *temp, dlistint_t **head)
 {
 	temp->next->prev = NULL;
 	*head = temp->next;
+	free(temp);
+	temp = NULL;
 	return (1);
 }
 
+/**
+ * delete_at_start_emptylist - deletes node at start of empty list
+ * @head: pointer to head
+ * Return: 1 for success
+ */
 int delete_at_start_emptylist(dlistint_t **head)
 {
 	free(*head);
@@ -65,17 +78,30 @@ int delete_at_start_emptylist(dlistint_t **head)
 	return (1);
 }
 
+/**
+ * delete_at_end - deletes node at end
+ * @temp: at indexed position for node to delete
+ * Return: 1 for success
+ */
 int delete_at_end(dlistint_t *temp)
 {
 	temp->prev->next = NULL;
+	free(temp);
+	temp = NULL;
 	return (1);
 }
-
+/**
+ * delete_at_index - deletes node at index
+ * @temp: at indexed position for node to delete
+ * Return: 1 for success
+ */
 int delete_at_index(dlistint_t *temp)
 {
 	/*prev needs to be temp*/
 	temp->prev->next = temp->next;
 	/*next needs to be temp*/
 	temp->next->prev = temp->prev;
+	free(temp);
+	temp = NULL;
 	return (1);
 }
