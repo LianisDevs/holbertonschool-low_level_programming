5192cbeb91828f1aaabb13fd2186015a13200b80
LianisDevs <lianisdevs@gmail.com>
2025-12-08 20:05:03 +1100

    Updated 4- added while loop to traverse list

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -13,6 +13,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
 	int key_check = 1;
+	hash_node_t *temp;
 
 	if (ht == NULL || key == NULL)
 		return (NULL);
@@ -24,11 +25,17 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (ht->array[index] == NULL)
 		return (NULL);
 
-	/*checking if key is in array*/
-	key_check = strcmp(ht->array[index]->key, key);
+	/*setting temp to be current index in array so we can traverse list*/
+	temp = ht->array[index];
 
-	if (key_check == 0)
-		return (ht->array[index]->value);
+	while (temp != NULL)
+	{
+		/*checking if key is in array*/
+		key_check = strcmp(ht->array[index]->key, key);
 
+		if (key_check == 0)
+			return (ht->array[index]->value);
+		temp = temp->next;
+	}
 	return (NULL);
 }
