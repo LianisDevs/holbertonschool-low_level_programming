8a2c4a99a5a56d053c1641d05963f9fe008b4e28
LianisDevs <lianisdevs@gmail.com>
2025-12-08 18:17:13 +1100

    Updated 3-added if statement for if ht is null and cleaned up updating value

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,8 +3,8 @@
 #include <string.h>
 #include "hash_tables.h"
 
-int setup(const char *key, const char *value, hash_node_t *new_node);
-void key_same(const char *value, hash_table_t *ht, unsigned long int index);
+hash_node_t *setup(const char *key, const char *value);
+int key_same(const char *value, hash_table_t *ht, unsigned long int index);
 /**
  * hash_table_set - adds an element to the hash table
  * @ht: pointer to hashtable to add to
@@ -15,18 +15,22 @@ void key_same(const char *value, hash_table_t *ht, unsigned long int index);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
+	hash_node_t *new_node;
 
-	/*malloc new_node and check it was successful*/
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
-
-	if (new_node == NULL)
+	if (ht == NULL)
 		return (0);
 
 	/*get the key_index*/
 	index = key_index((const unsigned char *)key, ht->size);
 
+	/*checking if key exists already*/
+	if (ht->array[index] != NULL && ht->array[index]->key == key)
+		return (key_same(value, ht, index));
+
+	new_node = setup(key, value);
+
 	/*checking if setup function failed for fast return*/
-	if (setup(key, value, new_node) == 1)
+	if (new_node == NULL)
 		return (0);
 
 	/*if nothing at index position then add new node*/
@@ -36,13 +40,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	/*if index position already has a node then add new_node to front of list*/
 	else
 	{
-		/*checking if key already exists*/
-		if (ht->array[index]->key == key)
-		{
-			key_same(value, ht, index);
-			return (1);
-		}
-
 		new_node->next = ht->array[index];
 		ht->array[index] = new_node;
 	}
@@ -53,17 +50,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
  * setup - sets up new node
  * @key: key is the key
  * @value: value associated with key
- * @new_node: pointer to new node
- * Return: 0 on success 1 for failure
+ * Return: new_node or NULL on error
  */
-int setup(const char *key, const char *value, hash_node_t *new_node)
+hash_node_t *setup(const char *key, const char *value)
 {
+	/*malloc new_node and check it was successful*/
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
 	/*using strdup to copy and malloc key and value*/
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
 		free(new_node);
-		return (1);
+		return (NULL);
 	}
 
 	if (value == NULL)
@@ -75,11 +77,11 @@ int setup(const char *key, const char *value, hash_node_t *new_node)
 		{
 			free(new_node->key);
 			free(new_node);
-			return (1);
+			return (NULL);
 		}
 	}
 	new_node->next = NULL;
-	return (0);
+	return (new_node);
 }
 
 /**
@@ -89,9 +91,10 @@ int setup(const char *key, const char *value, hash_node_t *new_node)
  * @index: index position in array
  * Return: nothing/ void
  */
-void key_same(const char *value, hash_table_t *ht, unsigned long int index)
+int key_same(const char *value, hash_table_t *ht, unsigned long int index)
 {
 	/*need to clear current value then set new value*/
 	free(ht->array[index]->value);
 	ht->array[index]->value = strdup(value);
+	return (1);
 }
