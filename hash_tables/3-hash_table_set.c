c18e1108ba7e6583f020046903c6825ec68129c2
LianisDevs <lianisdevs@gmail.com>
2025-12-08 15:42:45 +1100

    Updated 3- forgot to remove a line that was moved elsewhere

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -42,10 +42,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	/*if index position already has a node then add new_node to front of list*/
 	else
 	{
-		if (ht->array[index]->key == key)
-
 		new_node->next = ht->array[index];
-
 		ht->array[index] = new_node;
 	}
 	return (1);
